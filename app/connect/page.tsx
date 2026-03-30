24084e4b45fbf4fca4ecd7ae0b964e49e06f0c63
JOBLUX <mzaourm@gmail.com>
2026-03-30 13:26:55 +0200

    fix: remove muted type error on connect page

--- app/connect/page.tsx
+++ app/connect/page.tsx
@@ -66,7 +66,7 @@ export default function ConnectPage() {
                   {item.link ? (
                     <Link href={item.link} className="text-[#888] underline underline-offset-4 decoration-[#444] hover:text-[#ccc] transition-colors">{item.text}</Link>
                   ) : (
-                    <span className={item.muted ? 'text-[#333] italic' : 'text-[#555]'}>{item.text}</span>
+                    <span className='text-[#555]'>{item.text}</span>
                   )}
                 </li>
               ))}
